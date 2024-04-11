5e2de2c18fe5087ae51a2d56a4c8ec1ef29ca484
Wandati <petersimon801@gmail.com>
2024-04-11 17:00:23 +0300

    rebuild

---
+++ BookList/api/views.py
@@ -0,0 +1,10 @@
+
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BookSerializer
+from .models import Book
+# Create your views here.
+
+class BookViewset(viewsets.ModelViewSet):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
