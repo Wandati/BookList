9af59dc8c0c7aae7db6176c7f2f2a591728096f0
Wandati <petersimon801@gmail.com>
2024-04-17 11:57:56 +0300

    changes

--- frontend/bookapp/src/App.jsx
+++ frontend/bookapp/src/App.jsx
@@ -15,17 +15,23 @@ function App() {
     isbn: "",
   });
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchBooks();
   }, []);
 
   const fetchBooks = async () => {
+    setLoading(true);
     try {
-      const response = await axios.get("https://booklist-tp5r.onrender.com/api/books/");
+      const response = await axios.get(
+        "https://booklist-tp5r.onrender.com/api/books/"
+      );
       setBooks(response.data);
+      setLoading(false);
     } catch (error) {
       setError("Failed to fetch books");
+      setLoading(false);
     }
   };
 
@@ -37,7 +43,10 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("https://booklist-tp5r.onrender.com/api/books/", newBook);
+      await axios.post(
+        "https://booklist-tp5r.onrender.com/api/books/",
+        newBook
+      );
       alert("Book added successfully");
       fetchBooks();
       setShowAddForm(false);
@@ -94,7 +103,9 @@ function App() {
     const book_id = Number(id);
     if (window.confirm("Are you sure you want to delete this book?")) {
       try {
-        await axios.delete(`https://booklist-tp5r.onrender.com/api/books/${book_id}/`);
+        await axios.delete(
+          `https://booklist-tp5r.onrender.com/api/books/${book_id}/`
+        );
         alert("Book deleted successfully");
         fetchBooks();
       } catch (error) {
@@ -135,235 +146,246 @@ function App() {
   return (
     <div className="container">
       <h1 className="text-center mt-5 mb-4">BooksApp</h1>
-
-      <div className="mb-3">
-        {!showUpdateForm && (
-          <button
-            className="btn btn-secondary mr-2"
-            onClick={() => setShowAddForm(!showAddForm)}>
-            Add A Book
-          </button>
-        )}
-      </div>
-      {showAddForm && (
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-md-6">
-              <div className="form-floating mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="title"
-                  placeholder="Title"
-                  name="title"
-                  value={newBook.title}
-                  onChange={handleInputChange}
-                  required
-                />
-                <label htmlFor="title">Title</label>
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="form-floating mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="author"
-                  placeholder="Author"
-                  name="author"
-                  value={newBook.author}
-                  onChange={handleInputChange}
-                  required
-                />
-                <label htmlFor="author">Author</label>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">
-              <div className="form-floating mb-3">
-                <input
-                  type="date"
-                  className="form-control"
-                  id="publication_date"
-                  name="publication_date"
-                  value={newBook.publication_date}
-                  onChange={handleInputChange}
-                  required
-                />
-                <label htmlFor="publication_date">Publication Date</label>
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="form-floating mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="isbn"
-                  placeholder="ISBN"
-                  name="isbn"
-                  value={newBook.isbn}
-                  onChange={handleInputChange}
-                  required
-                />
-                <label htmlFor="isbn">ISBN</label>
-              </div>
-            </div>
-          </div>
-          <button type="submit" className="btn btn-outline-secondary mr-2">
-            Submit
-          </button>
-          <button
-            type="button"
-            className="btn btn-dark m-4"
-            onClick={handleCancelAdd}>
-            Cancel
-          </button>
-        </form>
-      )}
-      {showUpdateForm && selectedBook && (
+      {loading ? (
+        <div className="text-center">
+          <p className="text-center">Fetching Data...</p>
+        </div>
+      ) : (
         <>
-          <h2 className="mb-4">Update Form</h2>
-          <form onSubmit={handleUpdateSubmit}>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-floating mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="updateTitle"
-                    placeholder="Title"
-                    name="title"
-                    value={newBook.title}
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <label htmlFor="updateTitle">Title</label>
+          <div className="mb-3">
+            {!showUpdateForm && (
+              <button
+                className="btn btn-secondary mr-2"
+                onClick={() => setShowAddForm(!showAddForm)}>
+                Add A Book
+              </button>
+            )}
+          </div>
+          {showAddForm && (
+            <form onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-floating mb-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="title"
+                      placeholder="Title"
+                      name="title"
+                      value={newBook.title}
+                      onChange={handleInputChange}
+                      required
+                    />
+                    <label htmlFor="title">Title</label>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-floating mb-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="author"
+                      placeholder="Author"
+                      name="author"
+                      value={newBook.author}
+                      onChange={handleInputChange}
+                      required
+                    />
+                    <label htmlFor="author">Author</label>
+                  </div>
                 </div>
               </div>
-              <div className="col-md-6">
-                <div className="form-floating mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="updateAuthor"
-                    placeholder="Author"
-                    name="author"
-                    value={newBook.author}
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <label htmlFor="updateAuthor">Author</label>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-floating mb-3">
+                    <input
+                      type="date"
+                      className="form-control"
+                      id="publication_date"
+                      name="publication_date"
+                      value={newBook.publication_date}
+                      onChange={handleInputChange}
+                      required
+                    />
+                    <label htmlFor="publication_date">Publication Date</label>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-floating mb-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="isbn"
+                      placeholder="ISBN"
+                      name="isbn"
+                      value={newBook.isbn}
+                      onChange={handleInputChange}
+                      required
+                    />
+                    <label htmlFor="isbn">ISBN</label>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-floating mb-3">
-                  <input
-                    type="date"
-                    className="form-control"
-                    id="updatePublicationDate"
-                    name="publication_date"
-                    value={newBook.publication_date}
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <label htmlFor="updatePublicationDate">
-                    Publication Date
-                  </label>
+              <button type="submit" className="btn btn-outline-secondary mr-2">
+                Submit
+              </button>
+              <button
+                type="button"
+                className="btn btn-dark m-4"
+                onClick={handleCancelAdd}>
+                Cancel
+              </button>
+            </form>
+          )}
+          {showUpdateForm && selectedBook && (
+            <>
+              <h2 className="mb-4">Update Form</h2>
+              <form onSubmit={handleUpdateSubmit}>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-floating mb-3">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="updateTitle"
+                        placeholder="Title"
+                        name="title"
+                        value={newBook.title}
+                        onChange={handleInputChange}
+                        required
+                      />
+                      <label htmlFor="updateTitle">Title</label>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-floating mb-3">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="updateAuthor"
+                        placeholder="Author"
+                        name="author"
+                        value={newBook.author}
+                        onChange={handleInputChange}
+                        required
+                      />
+                      <label htmlFor="updateAuthor">Author</label>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="col-md-6">
-                <div className="form-floating mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="updateISBN"
-                    placeholder="ISBN"
-                    name="isbn"
-                    value={newBook.isbn}
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <label htmlFor="updateISBN">ISBN</label>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-floating mb-3">
+                      <input
+                        type="date"
+                        className="form-control"
+                        id="updatePublicationDate"
+                        name="publication_date"
+                        value={newBook.publication_date}
+                        onChange={handleInputChange}
+                        required
+                      />
+                      <label htmlFor="updatePublicationDate">
+                        Publication Date
+                      </label>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-floating mb-3">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="updateISBN"
+                        placeholder="ISBN"
+                        name="isbn"
+                        value={newBook.isbn}
+                        onChange={handleInputChange}
+                        required
+                      />
+                      <label htmlFor="updateISBN">ISBN</label>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <button type="submit" className="btn btn-outline-secondary mr-2">
-              Update
-            </button>
-            <button
-              type="button"
-              className="btn btn-dark m-4"
-              onClick={handleCancelUpdate}>
-              Cancel
-            </button>
-          </form>
+                <button
+                  type="submit"
+                  className="btn btn-outline-secondary mr-2">
+                  Update
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-dark m-4"
+                  onClick={handleCancelUpdate}>
+                  Cancel
+                </button>
+              </form>
+            </>
+          )}
+
+          <div className="table-responsive">
+            <table className="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Author</th>
+                  <th>Publication Date</th>
+                  <th>ISBN</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody className="table-group-divider">
+                {currentBooks.map((book) => (
+                  <tr key={book.id}>
+                    <td>{book.title}</td>
+                    <td>{book.author}</td>
+                    <td>{book.publication_date}</td>
+                    <td>{book.isbn}</td>
+                    <td>
+                      <button
+                        className="btn btn-outline-secondary mr-2 btn-sm"
+                        onClick={() => handleUpdate(book)}>
+                        Update
+                      </button>
+                      <button
+                        className="btn btn-danger m-2 btn-sm"
+                        onClick={() => handleDelete(book.id)}>
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          <nav aria-label="Page navigation">
+            <ul
+              className="pagination justify-content-center"
+              style={{ color: "black" }}>
+              {Array.from(
+                { length: Math.ceil(books.length / booksPerPage) },
+                (_, i) => (
+                  <li
+                    className={`page-item ${
+                      currentPage === i + 1 ? "active" : ""
+                    }`}
+                    key={i + 1}>
+                    <button
+                      className="page-link"
+                      style={{
+                        backgroundColor: "white",
+                        color: "black",
+                        border: "1px solid gray",
+                      }}
+                      onClick={() => paginate(i + 1)}>
+                      {i + 1}
+                    </button>
+                  </li>
+                )
+              )}
+            </ul>
+          </nav>
         </>
       )}
-
-      <div className="table-responsive">
-        <table className="table table-striped table-hover">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author</th>
-              <th>Publication Date</th>
-              <th>ISBN</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody className="table-group-divider">
-            {currentBooks.map((book) => (
-              <tr key={book.id}>
-                <td>{book.title}</td>
-                <td>{book.author}</td>
-                <td>{book.publication_date}</td>
-                <td>{book.isbn}</td>
-                <td>
-                  <button
-                    className="btn btn-outline-secondary mr-2 btn-sm"
-                    onClick={() => handleUpdate(book)}>
-                    Update
-                  </button>
-                  <button
-                    className="btn btn-danger m-2 btn-sm"
-                    onClick={() => handleDelete(book.id)}>
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      <nav aria-label="Page navigation">
-        <ul
-          className="pagination justify-content-center"
-          style={{ color: "black" }}>
-          {Array.from(
-            { length: Math.ceil(books.length / booksPerPage) },
-            (_, i) => (
-              <li
-                className={`page-item ${currentPage === i + 1 ? "active" : ""}`}
-                key={i + 1}>
-                <button
-                  className="page-link"
-                  style={{
-                    backgroundColor: "white",
-                    color: "black",
-                    border: "1px solid gray",
-                  }}
-                  onClick={() => paginate(i + 1)}>
-                  {i + 1}
-                </button>
-              </li>
-            )
-          )}
-        </ul>
-      </nav>
     </div>
   );
 }
